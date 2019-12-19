08c25e1db53530cf355b9183d88120e6e436fd38
Raniaz881 <58309783+Raniaz881@users.noreply.github.com>
2019-12-19 22:44:22 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:home_page/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Ajar',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF33CCB7),
+        accentColor: Color(0xFFB2DFDB),
+        scaffoldBackgroundColor: Color(0xFFEBEDEF),
+
+
+      ),
+      home: HomeScreen(),
+
+
+
+    );
+  }
+
+}
+
+
+
+
